9cab78e30792acdfb5352ee9e969237a9ff75236
Gbemiga97 <holuwagbemiga97@gmail.com>
2023-11-07 15:34:27 +0100

    third commit

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,4 +1,4 @@
-import { data } from "../utils"
+import { data, variants } from "../utils"
 import { motion } from "framer-motion"
 
 
@@ -7,28 +7,42 @@ const About = () => {
   const {title, subtitle1, subtitle2, btnIcon, btnText} = data.aboutData
 
   return (
-    <section>
+    <section className="lg:py-16 xl:pb-[160px]">
       <div className="container mx-auto">
-        <div className="flex flex-col lg:">
+        <motion.div 
+        variants={variants.container}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{ once: true }}
+        className="flex flex-col lg:flex-row gap-x-[70px] items-center">
 
-          <div>
-            <div></div>
-          </div>
+          <motion.div 
+          variants={variants.fadeIn('right')}
+          className="flex flex-col lg:flex-row flex-1">
+            <div className='text-[360px] xl:text-[720px] leading-none font-tertiary lg:-tracking-[0.055em]
+            bg-about bg-no-repeat bg-right bg-clip-text text-transparent'>
+              01
+            </div>
+          </motion.div>
 
-          <div>
-              <h2>
+          <motion.div 
+          variants={variants.fadeIn('left')}
+          className="flex-1 h-full xl:mt-48">
+              <h2 className="h2">
                 {title}
                 </h2>
-                <div>
-                  <p>{subtitle1}</p>
-                  <p>{subtitle2}</p>
-                  <button>
-                    {btnText} <div>{btnIcon}</div>
+                <div className="flex flex-col items-end">
+                  <div className="max-w-[530px] text-grey">
+                  <p className="mb-6">{subtitle1}</p>
+                  <p className="mb-9">{subtitle2}</p>
+                  <button className="btn btn-lg btn-link">
+                    {btnText} <div className="text-xl">{btnIcon}</div>
                   </button>
+                  </div>
                 </div>
-          </div>
+          </motion.div>
 
-        </div>
+        </motion.div>
       </div>
     </section>
   )
--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,8 +1,114 @@
+import { useInView } from "react-intersection-observer"
+import { CircularProgressbar } from "react-circular-progressbar"
+import 'react-circular-progressbar/dist/styles.css'
+
+import { motion } from "framer-motion"
+import { useEffect, useState } from "react"
 
 
 const Skills = () => {
+
+  const {ref, inView} = useInView({
+    threshold: 0.2,
+  })
+
+
+  const [fullBody, setFullBody] = useState(0)
+  const [piercing, setPiercing] = useState(0)
+  const [fullColor, setFullColor] = useState(0)
+  const [temporary, setTemporary] = useState(0)
+
+  
+  useEffect(() => {
+    if(inView) {
+      setTimeout(() => {
+        if(fullBody < 90) {
+          setFullBody(prev => + prev + 1)
+        } 
+        if(piercing < 80) {
+          setPiercing(prev => prev + 1)
+        }
+        if(fullColor < 75) {
+          setFullColor(prev => prev + 1)
+        }
+        if(temporary < 95) {
+          setTemporary(prev => prev + 1)
+        }
+      }, 50)
+    } else {
+      setFullBody(0)
+      setPiercing(0)
+      setFullColor(0)
+      setTemporary(0)
+    }
+  }, [inView, fullBody, piercing, fullColor, temporary])
+
+  const styles = {
+    path : {
+      stroke: '#111111'
+    },
+    trail: {
+      stroke: '#eeeeee'
+    },
+    text: {
+      fill: '#111111',
+      fontSize: '24px'
+    }
+  }
   return (
-    <div>Skills</div>
+    <section ref={ref} className='section'>
+      <div className="container mx-auto">
+        <div className="flex flex-col md:flex-row justify-between items-center gap-y-6">
+          <div className="w-[150px] lg:w-[275px] flex flex-col items-center gap-y-6">
+            <CircularProgressbar
+            strokeWidth={1}
+            value={fullBody}
+            styles={styles}
+            text={`${fullBody}%`}
+            />
+            <p className="uppercase font-light translate-[1.2px] text-center">
+              full body tattoo
+            </p>
+          </div>
+
+          <div className="w-[150px] lg:w-[275px] flex flex-col items-center gap-y-6">
+            <CircularProgressbar
+            strokeWidth={1}
+            value={piercing}
+            styles={styles}
+            text={`${piercing}%`}
+            />
+            <p className="uppercase font-light translate-[1.2px] text-center">
+              safely piercing
+            </p>
+          </div>
+
+          <div className="w-[150px] lg:w-[275px] flex flex-col items-center gap-y-6">
+            <CircularProgressbar
+            strokeWidth={1}
+            value={fullColor}
+            styles={styles}
+            text={`${fullColor}%`}
+            />
+            <p className="uppercase font-light translate-[1.2px] text-center">
+              full color tattoo
+            </p>
+          </div>
+
+          <div className="w-[150px] lg:w-[275px] flex flex-col items-center gap-y-6">
+            <CircularProgressbar
+            strokeWidth={1}
+            value={temporary}
+            styles={styles}
+            text={`${temporary}%`}
+            />
+            <p className="uppercase font-light translate-[1.2px] text-center">
+              temporary tattoo
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
